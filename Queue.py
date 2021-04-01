f08fe750bad50f75a8a59553c5c426d4ede972a2
ianalexanderz <ianstewart745@gmail.com>
2021-04-01 11:30:31 -0700

    Implemented stack and queue

--- Queue.py
+++ Queue.py
@@ -1,45 +1,37 @@
 class Queue:
     def __init__(self):
-        pass
-
+        self.data = []
     def isEmpty(self):
         """Returns True if Queue is empty, False otherwise"""
-        pass
-
+        return self.data == []
     def enqueue(self, item):
         """Adds an item to the back of queue"""
-        pass
-
+        self.data.append(item)
+        print(self.data)
     def dequeue(self):
         """Removes the item from the front of queue. Returns the removed item"""
-        pass
-
+        return self.data.pop(0)
     def peek(self):
         """Returns the first item in the queue, but doesn't remove it"""
-        pass
-
+        return self.data[0]
     def size(self):
         """Returns the (int) size of the queue"""
-        pass
-
+        return len(self.data)
     def __str__(self):
         """Returns a string representation of all items in queue"""
-        return ""
-
-
+        new_string = ""
+        for i in range(len(self.data)):
+            new_string += self.data[i] + " "
+        return new_string
 ### TEST SUITE ###
 # Use the following code to help you implement the Queue
 my_queue = Queue()
-
 print('Is Queue empty?', my_queue.isEmpty())
-
 my_queue.enqueue('A')
 my_queue.enqueue('B')
 my_queue.enqueue('C')
 my_queue.enqueue('D')
-
 print('Dequeued item:', my_queue.dequeue())
 print('First item:', my_queue.peek())
-
 print('Here are all the items in the queue:', my_queue)
 print('The size of my stack is:', my_queue.size())
--- Stack.py
+++ Stack.py
@@ -1,30 +1,35 @@
 class Stack:
     def __init__(self):
-        pass
+        self.stack = []
+        
 
     def isEmpty(self):
         """Returns True if Stack is empty, False otherwise"""
-        pass
+        return self.stack == []
 
     def push(self, item):
         """Adds an item to the top of stack"""
-        pass
+        self.stack.append(item)
+        print(self.stack)
 
     def pop(self):
         """Removes the item from the top of stack. Returns the removed item"""
-        pass
+        return self.stack.pop()
 
     def peek(self):
         """Returns the top item in the stack, but doesn't remove it"""
-        pass
+        return self.stack[len(self.stack) - 1]
 
     def size(self):
         """Returns the (int) size of the stack"""
-        pass
+        return len(self.stack)
 
     def __str__(self):
         """Returns a string representation of all items in stack"""
-        return ''
+        new_string = ""
+        for i in range(len(self.stack)):
+            new_string += self.stack[i] + " "
+        return new_string
 
 ### TEST SUITE ###
 # Use the following code to help you implement the Stack
@@ -37,11 +42,28 @@ my_stack.push('B')
 my_stack.push('C')
 my_stack.push('D')
 
+
+
+
 print('Popped item should be D: \n', my_stack.pop())
 print('Top item should be C: \n', my_stack.peek())
 print('Here are all the items in the stack - Should be A B C: \n', my_stack)
-print('The size of stack should be 3: \n', my_stack.size())
+# print('The size of stack should be 3: \n', my_stack.size())
 
 # Bonus Question - How would our Stack implementation change if we
 # created it with a LinkedList instead of a List?
 # What is a LinkedList?
+
+
+
+
+
+# Driver program to test above functions   
+# Driver code
+# stack = Stack()
+# stack.push(10)
+# stack.push(20)
+# stack.push(30)
+ 
+# print "% d popped from stack" % (stack.pop())
+# print "Top element is % d " % (stack.peek())
